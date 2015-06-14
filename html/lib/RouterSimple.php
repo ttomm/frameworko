b1138a07e5284ee1d2056e1988e20bb93607e517
Tomek <tomasz.myhal@gmail.com>
2015-06-14 18:28:03 +0200

    RouterSImple

---
+++ html/lib/RouterSimple.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of RouterSimple
+ *
+ * @author Tomek
+ */
+class RouterSimple implements IRouter
+{
+    //put your code here
+}
